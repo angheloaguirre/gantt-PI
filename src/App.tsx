3ea468d873c61000defccf6b1e21acfed3f7944e
Team PI <procesoseinnovacionpi@gmail.com>
2026-09-22 16:25:58 -0500

    test normal

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
     downloadTemplate,
   } = useActivities();
 
-  const [projectName, setProjectName] = useState("Cronograma PI uwu");
+  const [projectName, setProjectName] = useState("Cronograma PI");
 
   return (
     <div className="flex flex-col h-screen bg-white overflow-hidden">
